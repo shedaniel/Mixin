62dbe65a063f622582f6ab1a8895f4dee79d0147
shedaniel <daniel@shedaniel.me>
2020-08-20 18:38:42 +0800

    tryd

--- src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
+++ src/main/java/org/spongepowered/asm/launch/MixinBootstrap.java
@@ -61,7 +61,7 @@ public abstract class MixinBootstrap {
     /**
      * Subsystem version
      */
-    public static final String VERSION = "0.8.1";
+    public static final String VERSION = "0.8.99999999";
     
     /**
      * Log all the things
